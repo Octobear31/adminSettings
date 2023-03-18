b4b89383aa463f743dfa1f3c2c7c1fc968affe70
Mykhailo Pinkevych <pinkevych23@gmail.com>
2023-03-18 20:30:03 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <div>
+        <TextField fullWidth label="fullWidth" id="fullWidth" />
+    </div>
+);
